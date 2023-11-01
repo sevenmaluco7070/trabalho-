847e35e5048291371fd2ba895f6c73305a1a7923
sevenmaluco7070 <129309165+sevenmaluco7070@users.noreply.github.com>
2023-11-01 12:17:06 -0300

    Add files via upload

---
+++ mains.js
@@ -0,0 +1,24 @@
+function tocaSom(idElementoAudio){
+document.querySelector(idElementoAudio).play();
+}
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+        
+        for(let contador = 0; contador < listaDeTeclas.length;contador++) {
+            const tecla = listaDeTeclas[contador];
+            const instrumento = tecla.classList[1];
+            const idAudio = `#som_${instrumento}`;
+            console.log(idAudio);
+            console.log(instrumento);
+
+            tecla.onclick = function(){
+               tocaSom(idAudio);  
+            }
+            tecla.onkeydown = function() {
+             tecla.classlist.add('ativa'); 
+            }
+            tecla.onkeyup = function(){
+                tecla.classlist.remove('ativa');
+            }
+            console.log(contador)
+      }
